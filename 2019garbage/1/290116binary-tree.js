f295f30bd56387f4a3af6a4bc22396e6ca22ca6c
Charles <774855001@qq.com>
2019-02-11 21:42:16 +0800

    0211

--- 2019garbage/1/290116binary-tree.js
+++ 2019garbage/1/290116binary-tree.js
@@ -12,14 +12,14 @@
 
 //左子树，右子树，根结点，叶子结点
 
-//二叉搜索树（Binary Search Tree),也称二叉搜索树、有序二叉树(ordered binary tree)，排序二叉树(sorted binary tree),是指一棵空树（null)或者具有下列性质的二叉树：
+//二叉搜索树（Binary Search Tree(BST),也称二叉搜索树、有序二叉树(ordered binary tree)，排序二叉树(sorted binary tree),是指一棵空树（null)或者具有下列性质的二叉树：
 //1.左子树 上所有结点的值均小于它的根结点的值（一定要整个左子树都要小于根结点）
 //2.右子树 上所有结点的值均大于等于它的根节点的值（一定要整个右子树都要大于等于根结点）
 //3.Recursively(递归)，左、右子树也分别满足二叉搜索树
 
 //满二叉树：每一层节点都是满的，满足2^(i-1)
 //完全二叉树：假如深度为h,从1~(h-1)层的节点都排满,第h层所有的结点都连续集中在最左边，这就是完全二叉树.
-//平衡二叉树
+//平衡二叉树:平衡二叉搜索树（英语：Balanced Binary Tree）是一种结构平衡的二叉搜索树，即叶节点高度差的绝对值不超过1，并且左右两个子树都是一棵平衡二叉树。它能在O(log n)内完成插入、查找和删除操作，最早被发明的平衡二叉搜索树为AVL树。
 
 //二叉树,结构递归，定义递归
 
@@ -37,18 +37,18 @@ var root = {
 
 //把根结点在rootPos位置的顺序储存的二叉树(数组)转换为一个链式二叉树（二叉链表）
 //递归顺序是一直算左子树直到为null然后返回
-function ary2tree(ary,rootPos = 0) {
-  if (ary.lenght == 0) {
+function ary2tree(ary,rootPos = 0) { //默认树根为0
+  if (ary.length == 0) {
     return null
   }
   if (rootPos > ary.length - 1) {
     return null
   }
-  if (ary[rootPos] === null) {
+  if (ary[rootPos] === null) {    //如果根结点为 Null 返回 null
     return null
   }
   var node = {
-    val:ary[rootPost],
+    val:ary[rootPos],
     left:null,
     right:null
   }
@@ -59,9 +59,9 @@ function ary2tree(ary,rootPos = 0) {
   return node
 }
 
-//简化后
+//简化版本
 function ary2tree(ary,rootPos = 0) {
-  if (ary.lenght == 0) {
+  if (ary.length == 0) {
     return null
   }
   if (rootPos > ary.length - 1) {
@@ -92,9 +92,44 @@ function tree2ary(root, ary = [], rootPos = 0) {
 }
 
 
-//leetcode 将数组转换成树 ?
-//队列方式
+//leetcode 将数组转换成树 (用一个额外数组来保存非空节点)
+//方法一
 function ary2treeLC(ary) {
+  if (ary.length == 0) {
+    return null
+  }
+  var rootNode = {  //声明根结点
+    val:ary[0],
+    left:null,
+    right:null
+  }
+  var queue = [rootNode]
+  var leftUsed = false
+  for (var i = 1;i < ary.length;i++) {
+    var val = ary[i]
+    if (val) {
+      var node = {
+        val: val,
+        left:null,
+        right:null
+      }
+      queue.push(node)
+    } else {
+      node = null
+    }
+    if (!leftUsed) {
+      queue[0].left = node
+      leftUsed = true
+    } else {
+      queue[0].right = node
+      leftUsed = false
+      queue.shift()
+    }
+  }
+  return rootNode
+}
+//方法二 队列方式
+function condensedArray2Tree(ary) {
   if (ary.length == 0) {
     return null
   }
@@ -105,24 +140,81 @@ function ary2treeLC(ary) {
     right:null
   }
   queue.push(root)
-  for(var val of ary) {
-    if (val) {
-      var node = {
-        val:val,
+  var node
+  for(var i = 1;i < ary.length;i++) {
+    node = queue.shift()
+    if (ary[i] != null) {
+      node.left = {
+        val:ary[i],
         left:null,
         right:null
       }
+      queue.push(node.left)
+    }
+    i++
+    if (ary[i] != null) {
+      node.right = {
+        val:ary[i],
+        left:null,
+        right:null
+      }
+      queue.push(node.right)
     }
   }
+  return root
+}
+
+//将LC树转换成数组
+function tree2aryLC(root) {
+  if (!root) {
+    return []
+  }
+  var result = []
+  var queue = [root]
+  while(queue.length) {
+    var node = queue.shift()
+    if (node) {
+      result.push(node.val)
+      queue.push(node.left,node.right)
+    } else {
+      result.push(null)
+    }
+  }
+  return result
 }
 
 
-//树的遍历
-function reOrderTraverse(root,action = console.log) {
+//树的遍历 tree traverse
+//先序遍历（先遍历根结点，再遍历左节点，右节点）
+//中序遍历 (左节点，中节点，右节点)
+//后序遍历（左节点，右节点，中节点）
+
+//先序遍历
+// 绕着轮廓走
+function preOrderTraverse(root,action = console.log) {
   if(root) {
     action(root.val)
-    preOrderTraverse(root.left)
-    preOrderTraverse(root.right)
+    preOrderTraverse(root.left,action)
+    preOrderTraverse(root.right,action)
+  }
+}
+
+//中序遍历
+// 用一条垂直的线扫描
+function inOrderTraverse(root,action = console.log) {
+  if(root.left) {
+    inOrderTraverse(root.left,action)
+    action(root.left)
+    inOrderTraverse(root.right,action)
+  }
+}
+
+//后序遍历
+function postOrderTraverse(root,action = console.log) {
+  if(root) {
+    postOrderTraverse(root.left,action)
+    postOrderTraverse(root.right,action)
+    action(root.val)
   }
 }
 
--- 2019garbage/1/410127map与set类型.js
+++ 2019garbage/1/410127map与set类型.js
@@ -1,9 +1,16 @@
 //ES6
 //map()类型
 var a = new map()
+a.set('x',1) //设置的值
+a.set('y',2)
 
+a.get('x')//1
 
-
-
+a.has('x')
+//可以 Map 任何东西
+a.set(2,8)
+a.set('2',8)
+a.set({},10)
+a.size //有多少映射
 
 //set()类型
