6a199a8e000b17f411a9fe0a4d07edf10466e98a
Charles <774855001@qq.com>
2019-04-13 21:11:38 +0800

    garbage

---
+++ 2019garbage/4/230413vue优化性能原理.js
@@ -0,0 +1,19 @@
+// 改变数据
+// 到模板函数 生成virtual dom 结构
+// 做diff 求差异   （o(n)) 的算法
+// 将差异patch 补丁 更改到dom上
+
+//key
+
+
+
+//jquery 更改数据直接操作 dom
+
+
+//react reconciliatioin（协调） diff算法
+//对于dom树 只对同级做对比
+
+
+
+//性能优化 +key值
+//原理  生命周期
