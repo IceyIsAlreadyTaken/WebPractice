c7a38932d02b5c5de97b49a1be6f671069c5c15e
Charles <774855001@qq.com>
2019-04-14 20:46:02 +0800

    garbage

---
+++ 2019garbage/4/240414将对象变成getter和setter.js
@@ -0,0 +1,29 @@
+function observer(obj) {
+  function isObject(val) {
+    return Object.prototype.toString.call(val) === '[object Object]'
+  }
+
+  var copy = {}
+
+  for(let key in obj) {
+    if (isObject(obj[key])) {
+      copy[key] = observer(obj[key])
+    } else {
+      copy[key] = obj[key]
+    }
+
+    Object.defineProperty(obj,key,{
+      get:function() {
+        return copy[key]
+      },
+      set:function(val) {
+        console.log('you are setting me!')
+        if (isObject(val)) {
+          val = observer(val)
+        }
+        copy[key] = val
+      }
+    })
+    
+  }
+}
